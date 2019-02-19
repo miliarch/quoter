b8097565e531961b8e900a7f4162ab9acee4e7c2
Marcus Bowman <miliarch.mb@gmail.com>
2019-02-18 23:16:34 -0800

    Adjusted output format and documented current example

--- tests/fetch_random_quote.py
+++ tests/fetch_random_quote.py
@@ -1,5 +1,15 @@
+''' Output example:
+{
+    "page_title": "George Harrison",
+    "page_anchor": "George_Harrison",
+    "page_url": "https://en.wikiquote.org/wiki/George_Harrison",
+    "quote": "I felt in love, not with anything or anybody in particular but with everything.\n",
+    "credit": "of first taking LSD, The Beatles Anthology (2000), p. 177"
+}
+'''
 from context import quoter
 from quoter import quoter
+import json
 
 quote_dict = quoter.select_random_quote()
-print(quote_dict)
+print(json.dumps(quote_dict, indent=4))
